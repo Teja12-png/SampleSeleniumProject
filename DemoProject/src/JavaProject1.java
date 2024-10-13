addbaee937efe7887f3fafdb3f73561f684686ac
Teja <tejarao524227@gmail.com>
2024-10-13 15:24:26 +0530

    This is commit modification

---
+++ DemoProject/src/JavaProject1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		Scanner scan = new Scanner(System.in); 
+
+	}
+
+}
---
+++ DemoProject/src/org/openqa/selenium/Ex1.java
@@ -0,0 +1,17 @@
+package org.openqa.selenium;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+
+}
